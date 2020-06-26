26ec925db23adf068ddeb210ccd2471b5b66786b
dark-flames <dark_flames@outlook.com>
2020-06-26 09:45:06 +0900

    test: add test

--- tests/test_enum.rs
+++ tests/test_enum.rs
@@ -1,5 +1,5 @@
 use iroha::ToTokens;
-use quote::quote;
+use quote::{quote, ToTokens};
 
 #[derive(ToTokens)]
 #[Iroha(mod_path="test")]
@@ -9,7 +9,14 @@ enum Test {
     C
 }
 
-fn get_string(value: Test) -> String {
+#[derive(ToTokens)]
+enum Test2 {
+    A,
+    B,
+    C
+}
+
+fn get_string<T: ToTokens>(value: T) -> String {
     let tokens = quote! {
         #value
     };
@@ -18,11 +25,20 @@ fn get_string(value: Test) -> String {
 }
 
 #[test]
-pub fn test_enum() {
+pub fn test_enum_with_path() {
     let a = get_string(Test::A);
     assert_eq!(a, "{test::Test::A}");
     let b = get_string(Test::B);
     assert_eq!(b, "{test::Test::B}");
     let c = get_string(Test::C);
     assert_eq!(c, "{test::Test::C}");
+}
+
+pub fn test_enum() {
+    let a = get_string(Test2::A);
+    assert_eq!(a, "{Test2::A}");
+    let b = get_string(Test2::B);
+    assert_eq!(b, "{Test2::B}");
+    let c = get_string(Test2::C);
+    assert_eq!(c, "{Test2::C}");
 }
